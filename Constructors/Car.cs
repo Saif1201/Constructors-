94864ce2aacac16a0e6e3d058ddc3a6d4d996ef3
User <User@DESKTOP-B739F08>
2024-11-05 19:19:22 +0530

    Add project files.

---
+++ Constructors/Car.cs
@@ -0,0 +1,57 @@
+﻿namespace Constructors
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        #region Parameterized Constrcutor
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        #endregion
+
+        #region Copy Constructor 
+        public Car(Car copy)
+        {
+            Make = copy.Make;
+            Model = copy.Model;
+            Year = copy.Year;
+        }
+        #endregion
+
+        #region Static Constructor 
+        static Car()
+        {
+            Console.WriteLine("This is My Car");
+        }
+        #endregion
+
+        #region Static method
+        public static void print()
+        {
+            Console.WriteLine("This is my static method, but you can see static constructor has been executed before me");
+        }
+        #endregion
+
+        #region Parameter-less(default) Constructor
+        public Car()
+        {
+            Make = "Toyota";
+            Model = "Fortuner";
+            Year = 2025;
+        }
+        #endregion
+
+        //#region Private Constructor
+        //private Car()
+        //{
+
+        //}
+        //#endregion
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using Constructors;
+
+Console.WriteLine("Hello, World!");
+
+Car car1 = new Car("Scorpio", "Mahindra", 2011);
+
+Car copyCar = new Car(car1);
+
+Car.print();
+
+// Instance of Car #Parameter-less
+Car car = new Car();
+
+Console.WriteLine("Please enter your Car Name");
+var make = Console.ReadLine();
+
+Console.WriteLine("Please enter your Car Model");
+var model = Console.ReadLine();
+
+Console.WriteLine("Please enter year");
+var year = Convert.ToInt32(Console.Read());
+
+// Instance of Car #Parameterised
+//Car car1 = new Car(make,model,year);
+
+Console.WriteLine($"Car name is {make} Car model is {model} Car year is {year}");
+
+Console.Read();
